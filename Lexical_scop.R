ca7e42234fd5890805f46ee41eac1b89ce8bf03d
mastersrushi <mastersrushi@gmail.com>
2019-06-30 21:19:48 +0530

    Lexical Scope

--- Lexical_scop.R
+++ Lexical_scop.R
@@ -1,3 +1,12 @@
+## Put comments here that give an overall description of what your
+## functions do
+## Our aim in this experiment is to write a pair of functions, namely, 
+## "makeCacheMatrix" and "cacheSolve" that cache the inverse of a matrix
+
+## Write a short comment describing this function
+## makeCacheMatrix is a function which creates a special "matrix" object that can 
+## cache its inverse for the input (which is an invertible square matrix)
+
 makeCacheMatrix <- function(x = matrix()) {
   
   inv <- NULL
@@ -10,6 +19,12 @@ makeCacheMatrix <- function(x = matrix()) {
   getinv <- function() inv
   list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
+
+## Write a short comment describing this function
+## cacheSolve is a function which computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve the 
+## inverse from the cache
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
@@ -24,6 +39,8 @@ cacheSolve <- function(x, ...) {
   inv
 }
 
+
+## ---------------Checking the program------------------------
 m <- matrix(rnorm(16),4,4)
 m1 <- makeCacheMatrix(m)
 cacheSolve(m1)
